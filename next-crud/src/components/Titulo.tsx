40727baeff2eb70abc45636afab4de156a0b8128
elievelton <elievelton@gmail.com>
2022-11-26 09:12:08 -0300

    Versão 1.0 crud

---
+++ next-crud/src/components/Titulo.tsx
@@ -0,0 +1,11 @@
+export default function Titulo(props){
+
+    return (
+        <div>
+            <h1>
+                {props.children}
+            </h1>
+            <hr/>
+        </div>
+    )
+}
